8372db3b13e49ab7320380b824af793a753b80cd
arifahmedozturk <46283143+arifahmedozturk@users.noreply.github.com>
2020-07-17 10:43:34 +0100

    Update ApplicationPresenter.kt

--- SharedCode/src/commonMain/kotlin/ApplicationPresenter.kt
+++ SharedCode/src/commonMain/kotlin/ApplicationPresenter.kt
@@ -1,9 +1,14 @@
 package com.jetbrains.handson.mpp.mobile
 
 import io.ktor.client.HttpClient
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.request.get
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
+import kotlinx.serialization.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
 
 class ApplicationPresenter: ApplicationContract.Presenter() {
 
@@ -20,7 +25,14 @@ class ApplicationPresenter: ApplicationContract.Presenter() {
     }
     private suspend fun getApiCall(originStation:String,destinationStation:String):String {
         val client = HttpClient()
-        val queryURL = "https://mobile-api-dev.lner.co.uk/v1/fares?originStation=$originStation&destinationStation=$destinationStation&viaStation=WFJ&outboundDateTime=2020-07-20T12%3A16%3A27.371%2B00%3A00&numberOfChildren=2&numberOfAdults=2&doSplitTicketing=false"
+        {
+            install(JsonFeature)
+            {
+                serializer = KotlinxSerializer()
+            }
+        }
+
+        val queryURL = "https://mobile-api-dev.lner.co.uk/v1/fares?originStation=$originStation&destinationStation=$destinationStation&viaStation=WFJ&outboundDateTime=2020-07-20T12%3A16%3A27.371%2B00%3A00&numberOfChildren=0&numberOfAdults=1&doSplitTicketing=false"
         val content = client.get<String>(queryURL)
         client.close()
         return content
@@ -31,10 +43,12 @@ class ApplicationPresenter: ApplicationContract.Presenter() {
         var view = this.view
         launch{
             result = getApiCall(originStation,destinationStation)
+            val response = Json(JsonConfiguration.Stable)
+                    .parse(Response.serializer(),result)
             if (view != null) {
-                view.setJourneys(result)
+                view.setJourneys(response)
             }
         }
-        return result
+        return ""
     }
 }
